bf1cdd2f36795c655f25c66181fefddd3eb6469d
ariados22 <ariados22@gmail.com>
2021-10-19 13:48:49 +0900

    20211019 1348 - max768 반응형 테스트

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+  const NOWON_CLASSNAME = "now-on";
+
   // gnb > sub opener
   function subOpener() {
     $(".menu-item").on("mouseover", function() {
@@ -10,8 +12,18 @@ $(document).ready(function () {
     })
   }
 
+  // max 768 > nav on {
+  function mobileNavOn() {
+    $(".ham-btn").on("click", function() {
+      $(this).siblings("nav").fadeToggle();
+      $(this).siblings("nav").children(".gnb").slideToggle(300, "swing");
+      $(this).toggleClass(NOWON_CLASSNAME);
+    })
+  }
+  
   function init() {
     subOpener();
+    mobileNavOn();
   }
 
   init();
