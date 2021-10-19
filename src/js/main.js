7c0293dfd2cf080bd4bd1aea172706b809acc1bc
ariados22 <ariados22@gmail.com>
2021-10-19 17:09:56 +0900

    20211019 1709 - 768 반응형 중간 테스틑

--- src/js/main.js
+++ src/js/main.js
@@ -15,9 +15,24 @@ $(document).ready(function () {
   // max 768 > nav on {
   function mobileNavOn() {
     $(".ham-btn").on("click", function() {
+      let subHeight = $(".sub-menu").outerHeight();
       $(this).siblings("nav").fadeToggle();
       $(this).siblings("nav").children(".gnb").slideToggle(300, "swing");
       $(this).toggleClass(NOWON_CLASSNAME);
+
+      if ($(".ham-btn").hasClass(NOWON_CLASSNAME)) {
+        $(".ham-btn").on("click", function() {
+          $(".sub-menu").css("height", "0");
+        })
+
+        $("nav").on("scroll touchmove mousewheel", function (event) {
+          event.preventDefault();
+          event.stopPropagation();
+          return false;
+        })
+      } else {
+        $("nav").off("svroll touchmove mousewheel");
+      }
     })
   }
   
